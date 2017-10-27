0cbdcbae3d7d8f82731c304eb9d0356cb5ce503f
Nicolas Zin <nicolas.zin@savoirfairelinux.com>
2017-10-26 21:41:55 -0400

    add test code

---
+++ vannetest/vannetest.ino
@@ -0,0 +1,43 @@
+#include <ESP8266WiFi.h>
+
+#include "DHT.h"
+
+#define DHTPIN 2     // what digital pin we're connected to
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+ 
+void setup() {
+  dht.begin();
+  pinMode(16, OUTPUT);
+  digitalWrite(16, HIGH);
+}
+
+void loop() {
+  /*
+   * Read the sensor
+   */
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  if (t > 15) {
+    digitalWrite(16, HIGH);
+  } else {
+    digitalWrite(16, LOW);
+  }
+  delay(5000);
+}
+
